8dddbbd68ae444dde9030e212e2f7af1232e808d
David Neuy <davidneuy@Davids-MBP.fritz.box>
2018-11-11 15:19:43 +0100

    Initial Login Screen. #1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import Login from './Login'
 
 class App extends Component {
+  state = { currentUser: {} }
+
+  handleUserChange = (id, name) => {
+    this.setState({
+      currentUser: { id, name }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            test
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <Login currentUser={this.state.currentUser.id} onUserChange={this.handleUserChange} />
+      {/*
+      <header className="App-header">
+        <p>
+          test
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      */}
+    </div>
     );
   }
 }
---
+++ src/components/Login.js
@@ -0,0 +1,79 @@
+import React, { Component, Fragment } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Avatar from '@material-ui/core/Avatar';
+import * as data from '../utils/_DATA'
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    'justify-content': 'center',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+});
+
+class Login extends Component {
+  state = {
+    users: {}
+  };
+
+  componentDidMount() {
+    data._getUsers().then(users => {
+      this.setState({
+        users
+      });
+    })
+  }
+
+  handleChange = event => {
+    const userId = event.target.value
+    const userName = this.state.users[userId].name
+    this.props.onUserChange(userId, userName)
+  };
+
+  render() {
+    const { classes, currentUser='' } = this.props;
+    const { users } = this.state;
+
+    return (
+      <Fragment>
+        <form className={classes.root} autoComplete="off">
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="user">User</InputLabel>
+            <Select
+              value={currentUser}
+              onChange={this.handleChange}
+              autoWidth={true}
+              inputProps={{
+                id: 'user',
+              }}>
+             {Object.values(users).map((user) => (
+                 <MenuItem key={user.id} value={user.id}>
+                   <Avatar src={user.avatarURL} className={classes.avatar} style={{marginRight: 10}} component='span' /> {user.name}
+                 </MenuItem>
+               ))}
+            </Select>
+          </FormControl>
+        </form>
+
+        <div>
+          Icons made by
+          <a href="https://www.flaticon.com/authors/freepik" title="Man">Man</a>
+          from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+          is licensed by
+          <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a>
+        </div>
+
+      </Fragment>
+    )
+  }
+}
+
+export default withStyles(styles)(Login)
