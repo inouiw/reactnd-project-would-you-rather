e3808bad8b7a9eb89ee47e033e2ec29c62ddffe8
David Neuy <davidneuy@Davids-MBP.fritz.box>
2018-11-26 18:22:34 +0100

    Publish build.

---
+++ build/precache-manifest.29e4fa36b75b10b10ca9d7dce6986098.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c8197bf4349bedee7ad7",
+    "url": "/static/js/main.c8197bf4.chunk.js"
+  },
+  {
+    "revision": "f8d81f8988040588a42e",
+    "url": "/static/js/1.f8d81f89.chunk.js"
+  },
+  {
+    "revision": "c8197bf4349bedee7ad7",
+    "url": "/static/css/main.a54b6ff4.chunk.css"
+  },
+  {
+    "revision": "baacffc59652cc62cde22065be436aa4",
+    "url": "/index.html"
+  }
+];
